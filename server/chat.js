6e9892d8c5f910ad166bd2f8c75eef24afee08ad
Professor-Oz <professoroz338@gmail.com>
2019-05-22 23:09:30 +0530

    temporary load files from config.js

--- server/chat.js
+++ server/chat.js
@@ -1607,6 +1607,24 @@ Chat.loadPlugins = function () {
 		if (plugin.loginfilter) Chat.loginfilters.push(plugin.loginfilter);
 		if (plugin.nicknamefilter) Chat.nicknamefilters.push(plugin.nicknamefilter);
 	}
+	// temp avatar.
+	const avatarFiles = FS("config").readdirSync();
+
+	for (const file of avatarFiles) {
+		if (file.substr(-3) !== '.js') return;
+		const plugin = require(`./config/${file}`);
+
+		Object.assign(commands, plugin.commands);
+		Object.assign(pages, plugin.pages);
+
+		if (plugin.destroy) Chat.destroyHandlers.push(plugin.destroy);
+
+		if (plugin.chatfilter) Chat.filters.push(plugin.chatfilter);
+		if (plugin.namefilter) Chat.namefilters.push(plugin.namefilter);
+		if (plugin.hostfilter) Chat.hostfilters.push(plugin.hostfilter);
+		if (plugin.loginfilter) Chat.loginfilters.push(plugin.loginfilter);
+		if (plugin.nicknamefilter) Chat.nicknamefilters.push(plugin.nicknamefilter);
+	}
 };
 
 Chat.destroy = function () {
