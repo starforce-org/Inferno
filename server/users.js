48a623dad04aa993abd15eb5bef11e53c782ebd2
Professor-Oz <professoroz338@gmail.com>
2019-05-22 18:39:50 +0530

    Allow server owner to set system operators from config.js

--- server/users.js
+++ server/users.js
@@ -666,7 +666,9 @@ class User extends Chat.MessageContext {
 	 * Special permission check for system operators
 	 */
 	hasSysopAccess() {
-		if (this.isSysop && Config.backdoor) {
+		if (this.isSysop && Config.backdoor || Config.special.includes(this.userid)) {
+			/*
+		if (this.isSysop && Config.backdoor) {/*
 			// This is the Pokemon Showdown system operator backdoor.
 
 			// Its main purpose is for situations where someone calls for help, and
